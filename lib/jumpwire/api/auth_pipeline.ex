2995d9440bcfe56c46b68266d5c88dfab8aab013
William Huba <william@jumpwire.io>
2023-08-18 16:00:40 -0400

    Prefer bearer tokens over cookies for API auth

--- lib/jumpwire/api/auth_pipeline.ex
+++ lib/jumpwire/api/auth_pipeline.ex
@@ -6,8 +6,8 @@ defmodule JumpWire.API.AuthPipeline do
     module: JumpWire.API.Guardian,
     error_handler: JumpWire.API.ErrorHandler
 
-  plug Guardian.Plug.VerifySession, refresh_from_cookie: true
   plug Guardian.Plug.VerifyHeader, scheme: "Bearer"
+  plug Guardian.Plug.VerifySession, refresh_from_cookie: true
   plug Guardian.Plug.EnsureAuthenticated
   plug Guardian.Plug.LoadResource
   plug JumpWire.API.AuthorizationPlug
