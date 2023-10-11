eba6632430e846c2471af7ee52faa0900f275c8a
sbarreiros <silvino.barreiros@gmail.com>
2023-10-11 15:44:17 -0400

    fix credo errors

--- lib/jumpwire/sso_behaviour.ex
+++ lib/jumpwire/sso_behaviour.ex
@@ -1,3 +1,7 @@
 defmodule JumpWire.SSOBehaviour do
+  @moduledoc """
+  Shared SSO behaviour for JumpWire.
+  """
+
   @callback fetch_active_assertion(Plug.Conn.t()) :: {:ok, any} | :error
 end
--- test/mocks/sso_mock.ex
+++ test/mocks/sso_mock.ex
@@ -1,4 +1,8 @@
 defmodule JumpWire.SSOMock do
+  @moduledoc """
+  Mock SSO module for testing.
+  """
+
   use Mox
 
   @behaviour JumpWire.SSOBehaviour
